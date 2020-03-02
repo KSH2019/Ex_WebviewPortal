917025518966b08a6e9e9f718a9e8458757492ba
KSH2019 <shkim9533@korea.ac.kr>
2020-03-02 14:29:03 +0900

    3rd Commit! WebView로 고려대학교 포털&블랙보드 로그인 성공.

--- app/src/main/java/ku/winter20/androidwebwithwebview/LoginActivity.java
+++ app/src/main/java/ku/winter20/androidwebwithwebview/LoginActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
@@ -25,15 +26,18 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View arg0) {
                 username = ( (EditText) findViewById(R.id.username)).getText().toString();
                 password = ( (EditText) findViewById(R.id.password)).getText().toString();
-                doLogin(username, password);
+                doLoginToBB(username, password);
             }
         });
     }
 
-    private void doLogin(final String username, final String password) {
+    private void doLoginToPortal(final String username, final String password) {
         final WebView loginWebview = findViewById(R.id.loginWebview);
 
-        loginWebview.getSettings().setJavaScriptEnabled(true);
+        WebSettings loginWebSettings = loginWebview.getSettings();
+        loginWebSettings.setJavaScriptEnabled(true);
+        loginWebSettings.setDomStorageEnabled(true);
+
         loginWebview.addJavascriptInterface(new MyJavascriptInterface(), "Android");
 
         loginWebview.setWebViewClient(new WebViewClient() {
@@ -54,6 +58,39 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         loginWebview.loadUrl("https://portal.korea.ac.kr/");
+
+        Log.d(TAG, "username: " + username + "   password :" + password);
+    }
+
+    private void doLoginToBB(final String username, final String password) {
+        final WebView loginWebview = findViewById(R.id.loginWebview);
+
+        WebSettings loginWebSettings = loginWebview.getSettings();
+        loginWebSettings.setJavaScriptEnabled(true);
+        loginWebSettings.setDomStorageEnabled(true);
+
+        loginWebview.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+
+        loginWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+                String curURL = loginWebview.getUrl();
+
+                if (curURL.equals("https://sso.korea.ac.kr/saml/Auth.do")) {
+                    view.loadUrl("javascript:(function() { " +
+                            "document.getElementById('one_id').value = '" + username + "';" +
+                            "document.querySelector('input[name=\"user_password\"]').value = '" + password + "';" +
+                            "document.querySelector('div.form-button > div:not(hidden) > button').click();" +
+                            "})()");
+                }
+
+                Log.d(TAG, curURL);
+            }
+        });
+
+        loginWebview.loadUrl("http://kulms.korea.ac.kr/");
 
         Log.d(TAG, "username: " + username + "   password :" + password);
     }
